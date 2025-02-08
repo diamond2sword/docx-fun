feee203ba58c331cdf8b753590f3f4530f57aa66
diamond2sword <diamond2sword@gmail.com>
2025-02-09 00:36:06 +0800

    update project

---
+++ install_docx.bash
@@ -0,0 +1,22 @@
+{
+	#pythondocx
+	# pkg install tur-repo #for pandss?
+	# 	
+	# pkg install python-numpy
+	# pkg install python-pandas
+	# pip install pillow
+	{
+		#python docx
+		# apt install clang
+		# apt install libxml2
+		# apt install libxslt
+		# pip install cython
+		pkg install python-lxml
+		# export CFLAGS="-Wno-incompatible-function-pointer-types -Wno-implicit-function-declaration"	pip install lxml
+		pip install python-docx
+	}
+	# #pythondocx misc
+	# pip install roman
+	# pip install titlecase
+}
+
